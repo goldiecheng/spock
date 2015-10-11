fd0be73ae6311c72695caf183c1a31e776306840
Gabriel Rinaldi <gabriel_rinaldi@mac.com>
2015-10-10 23:52:32 -0700

    Added missing if statement to unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -15,7 +15,7 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base)
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
     Rails.logger.info('Disconnected from ActiveRecord')
   end
@@ -26,7 +26,7 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base)
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
     Rails.logger.info('Connected to ActiveRecord')
   end
